2d6ff2f51763c4f3088d2dc800d915727cb23687
Shoma Okamoto <shoma.develop@gmail.com>
2020-04-09 11:31:18 +0900

    Add sample code.

--- tasks/sample.py
+++ tasks/sample.py
@@ -1,1 +1,16 @@
 import luigi
+
+class TaskA(luigi.Task):
+    def run(self):
+        print("Hello, I'm TaskA.")
+
+class TaskB(luigi.Task):
+    def requires(self):
+        return TaskA()
+    
+    def run(self):
+        print("Hello, I'm TaskB.")
+
+
+if __name__ == '__main__':
+    luigi.run()
